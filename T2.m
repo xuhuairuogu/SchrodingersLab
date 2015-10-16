0606f6505ba3126848ca975b5440b247fed84569
Omar Ashour <omar.ashour22@gmail.com>
2015-10-16 17:40:44 +0300

    FIX

--- T2.m
+++ T2.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 % Copyright 2015, Omar Ashour.
 % This sourcecode is available from <https://github.com/oashour/HighNLSE/>
 %
@@ -39,54 +38,3 @@ psi = exp(-1i * dt/2 * pot).*psi;        % nonlinear calculation
 
 end
 
-=======
-% Copyright 2015, Omar Ashour.
-% This sourcecode is available from <https://github.com/oashour/HighNLSE/>
-%
-% This file is part of HighNLSE.
-% 
-% HighNLSE is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% HighNLSE is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-% 
-% You should have received a copy of the GNU General Public License
-% along with HighNLSE.  If not, see <http://www.gnu.org/licenses/>.
-
-function [psi] = T2(psi, dt, k2, gamma)
-% T2:
-%   This function calculates one time step using a second order split step
-%   algorithm. 
-%   OUTPUT:
-%       psi: evolution after one time step.
-%   INPUT:
-%       psi: function in spatial domain (not fft'd).
-%       dt:  temporal separation between nodes.
-%       k2:  square of wave numbers. Do not fft shift this.
-%       gamma: nonlinearity.
-%
-%   FORTRAN CODE:
-%       epot=agp*cdabs(psi)**2
-%       phi = cdexp( -im * 0.5d0*dt * epot )*psi
-%       call fft (phi, m, 0 )
-%       phi =cdexp( -im * dt * k2/2 )*phi
-%       call fft (phi, m, 1 )
-%       epot=agp*cdabs(phi)**2
-%       psi = cdexp( -im * 0.5d0*dt * epot )*phi
-
-psi2 = gamma*abs(psi).^2;                % Calculate potential
-psi = exp(-1i * dt/2 * psi2).*psi;       % Nonlinear calculation
-psi = fftshift(fft(psi));                % FFT
-psi = exp(-1i * dt * k2/2).*psi;         % Linear calculation
-psi = ifft(fftshift(psi));               % Inverse FFT
-psi2 = gamma*abs(psi).^2;                % Calculate potential
-psi = exp(-1i * dt/2 * psi2).*psi;       % nonlinear calculation
-
-end
-
->>>>>>> b1dd83ec248733620cb0c9a730f48c044a9dbebe
--- energy.m
+++ energy.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 % Copyright 2015, Omar Ashour.
 % This sourcecode is available from <https://github.com/oashour/HighNLSE/>
 %
@@ -59,65 +58,3 @@ title(sprintf('dt = %0.4f', dt))                    % Title
 error = sum(dE)*dt;                                 % integrated energy error
 end
 
-=======
-% Copyright 2015, Omar Ashour.
-% This sourcecode is available from <https://github.com/oashour/HighNLSE/>
-%
-% This file is part of HighNLSE.
-% 
-% HighNLSE is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-% 
-% HighNLSE is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-% 
-% You should have received a copy of the GNU General Public License
-% along with HighNLSE.  If not, see <http://www.gnu.org/licenses/>.
-
-function [error, E, ke, pe] = energy(psi, t, k2, Nx, gamma, dt)
-% ENERGY: finds the change in energy dE throughout the whole simulation
-% INPUT:
-%       psi: a matrix with the wave function evolution in space and time.
-%            rows are time, columns are space.
-%       t: time array
-%       k2: square of wave number
-%       Nx: number of Fourier modes/spatial nodes
-%       gamma: strength of the nonlinearity
-%       dt: temporal separation
-% OUTPUT:
-%       error: integrated energy error
-%       E: array with energy at every time step
-%       ke: array with kinetic energy at every time step
-%       pe: array with potential energy at every time step
-
-
-pe = zeros(1, length(t));                           % Preallocate pe
-ke = zeros(1, length(t));                           % Preallocate ke
-
-for i = 1:length(t)                                 % loop over time
-    psi2 = abs(psi(i, :)').*abs(psi(i, :)');        % square psi
-    pe(i) = sum(0.5*gamma*psi2.*psi2)/sum(psi2);    % calculate PE
-
-    psi_k = fftshift(fft(psi(i, :)'))/Nx;           % Calculate normalized FFT
-    psi_k2 = abs(psi_k).*abs(psi_k);                % Square normalized FFT
-    ke(i) =  sum(0.5*k2.*psi_k2)/sum(psi_k2);       % Calculate ke
-end
-
-E = ke + pe;                                        % Sum PE and KE to get E
-
-E0 = E(1);                                          % Initial energy
-dE = E-E0;                                          % Energy error
-
-figure
-plot(t, dE, '-b', 'LineWidth', 2)                   % Plot
-xlabel('t'); ylabel('E-E0');                        % Legend
-title(sprintf('dt = %0.4f', dt))                    % Title
-
-error = sum(dE)*dt;                                 % integrated energy error
-end
-
->>>>>>> b1dd83ec248733620cb0c9a730f48c044a9dbebe
